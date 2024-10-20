6bda2f430d0f7453ed284af90ca8b38911d95cba
Sanjay <sanjaysinghrajan@gmail.com>
2024-10-20 18:39:45 +0530

    Update Firebase Configuration

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASyteMPWSpdB9AGO036OlQn9SrKQ_P36Y",
-  authDomain: "portfolio-2f58f.firebaseapp.com",
-  projectId: "portfolio-2f58f",
-  storageBucket: "portfolio-2f58f.appspot.com",
-  messagingSenderId: "1043581390055",
-  appId: "1:1043581390055:web:21d45176d8cbb5dcde2642",
-  measurementId: "G-5KW4503QDQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
